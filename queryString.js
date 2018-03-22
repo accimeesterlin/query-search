ea73e690dcaed188296d2d52ce85617d3fb19f8b
Esterling Accime <esterling@brainbytescreative.com>
2018-03-22 00:37:59 -0400

    Minify Version

--- queryString.js
+++ queryString.js
@@ -1,14 +1,2 @@
-
-const convertQueryToObject = function (url) {
-    const arr = url.slice(1).split(/&|=/); // remove the "?", "&" and "="
-    const params = {};
-    for (var i = 0; i < arr.length; i += 2) {
-        const key = arr[i], value = arr[i + 1];
-        params[key] = value; // build the object = { limit: "10", page:"1", status:"APPROVED" }
-    }
-    return params;
-};
-
-
-
-module.exports = convertQueryToObject;
+var convertQueryToObject=function(url){var arr=url.slice(1).split(/&|=/);var params={};for(var i=0;i<arr.length;i+=2)
+{var key=arr[i],value=arr[i+1];params[key]=value}return params};module.exports=convertQueryToObject;
