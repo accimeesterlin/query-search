b03403fcf3226cd6a402aacdfc1ab4c7e2f8787a
Rafael Santos <rafaelnsantos@users.noreply.github.com>
2018-03-21 21:02:36 -0300

    Update queryString.js

--- queryString.js
+++ queryString.js
@@ -1,8 +1,8 @@
 
 const convertQueryToObject = (url) => {
     const arr = url.slice(1).split(/&|=/); // remove the "?", "&" and "="
-    let params = {};
-    for (let i = 0; i < arr.length; i += 2) {
+    const params = {};
+    for (var i = 0; i < arr.length; i += 2) {
         const key = arr[i], value = arr[i + 1];
         params[key] = value; // build the object = { limit: "10", page:"1", status:"APPROVED" }
     }
